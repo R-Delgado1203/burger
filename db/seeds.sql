dda486107bb00a2abc80547ed15f28681997fd7c
RolandoD <r.delgado1203@gmail.com>
2018-11-07 06:02:39 -0700

    app partially complete, requires html button configurations

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES("cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES("bacon burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("jalapeno burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("mushroom burger", false);
