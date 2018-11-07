3b2be00a2b9a6cdf0c70152a352bd1f77b2172ff
RolandoD <r.delgado1203@gmail.com>
2018-11-07 06:03:05 -0700

    app partially complete, requires html button configurations

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -20,7 +20,7 @@ $(function () {
         );
     });
 
-    $(".create-form").on("submit", function(event) {
+    $(".create-form").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
