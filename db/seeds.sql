86582e691f4c0f6fa8bfa4740c401fe3543a981c
RolandoD <r.delgado1203@gmail.com>
2018-11-08 01:52:04 -0700

    updated styling and certain html elements

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,4 @@ INSERT INTO burgers (burger_name, devoured) VALUES("hamburger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES("bacon burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES("jalapeno burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES("mushroom burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES("macaroni burger", false);
